a2c6730f7ea1552cd2c4f4b687c1385e126fe2db
DangerousDaniel <danielcox996@gmail.com>
2023-11-11 00:19:52 -0400

    Inventory Manager add and remove for stack version (has not been tested)

--- game/Inventory/PlayerInventory.gd
+++ game/Inventory/PlayerInventory.gd
@@ -2,12 +2,18 @@ class_name PlayerInventory extends Node2D
 
 @export var inventory : Array[InventoryItem]
 
-#func add_item(item : ItemData):
-#	inventory.append(item)
+func add_item(item : ItemData):
+	var inventory_item : InventoryItem
+	inventory_item.item_data = item
+	inventory_item.stackszie += 1
+	inventory.append(inventory_item)
 	
-#func remove_item(item : ItemData):
-#	for inventory_item in inventory:
-#		if item == inventory_item:
-#			inventory.erase(inventory)
+func remove_item(item : ItemData):
+	for inventory_item in inventory:
+		if inventory_item.item_data == item:
+			if inventory_item.stackszie == 0:
+				inventory.erase(inventory_item)
+			else:
+				inventory_item.stackszie -= 1
 
 
