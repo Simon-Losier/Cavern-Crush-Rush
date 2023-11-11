254fd804dadb542873bc4e48ac97a6f0db30da7b
DangerousDaniel <danielcox996@gmail.com>
2023-11-11 00:09:33 -0400

    stack version

---
+++ game/Inventory/InventoryItem.gd
@@ -0,0 +1,4 @@
+class_name InventoryItem extends Resource
+
+@export var item_data : ItemData
+@export var stackszie : int
--- game/Inventory/PlayerInventory.gd
+++ game/Inventory/PlayerInventory.gd
@@ -1,13 +1,13 @@
 class_name PlayerInventory extends Node2D
 
-@export var inventory : Array[ItemData]
+@export var inventory : Array[InventoryItem]
 
-func add_item(item : ItemData):
-	inventory.append(item)
+#func add_item(item : ItemData):
+#	inventory.append(item)
 	
-func remove_item(item : ItemData):
-	for inventory_item in inventory:
-		if item == inventory_item:
-			inventory.erase(inventory)
+#func remove_item(item : ItemData):
+#	for inventory_item in inventory:
+#		if item == inventory_item:
+#			inventory.erase(inventory)
 
 
