c5d33f3641029d16ccd84e681fa4cd50d1e0edd8
DangerousDaniel <danielcox996@gmail.com>
2023-11-11 09:44:39 -0400

    add update with check for the item if already in the stack

--- game/Inventory/InventoryItem.gd
+++ game/Inventory/InventoryItem.gd
@@ -2,3 +2,4 @@ class_name InventoryItem extends Resource
 
 @export var item_data : ItemData
 @export var stackszie : int
+@export var max_stacksize : int
--- game/Inventory/PlayerInventory.gd
+++ game/Inventory/PlayerInventory.gd
@@ -3,11 +3,15 @@ class_name PlayerInventory extends Node2D
 @export var inventory : Array[InventoryItem]
 
 func add_item(item : ItemData):
-	var inventory_item : InventoryItem
-	inventory_item.item_data = item
-	inventory_item.stackszie += 1
-	inventory.append(inventory_item)
-	
+	for inventory_item in inventory:
+		if inventory_item.item_data == item && inventory_item.stackszie != inventory_item.max_stacksize:
+			inventory_item.stackszie += 1
+		else:
+			var new_inventory_item : InventoryItem
+			new_inventory_item.item_data = item
+			new_inventory_item.stackszie += 1
+			inventory.append(new_inventory_item)
+			
 func remove_item(item : ItemData):
 	for inventory_item in inventory:
 		if inventory_item.item_data == item:
