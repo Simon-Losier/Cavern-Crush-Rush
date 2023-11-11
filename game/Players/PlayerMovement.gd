05c648ed900afc81ae9500646ea4f717e6231606
Alex Stewart <thealexstewart@protonmail.ch>
2023-11-10 21:33:28 -0400

    Players can now move properly, and are in Starter level.tscn

--- game/Players/PlayerMovement.gd
+++ game/Players/PlayerMovement.gd
@@ -1,25 +1,21 @@
 extends CharacterBody2D
 
 ## Constants
-const MOVEMENT_SPEED = 2.0
+const MOVEMENT_SPEED = 200.0
 const SLOWDOWN = 10.0
 
 ## Variables
 @export var is_player_1 : bool = true
 
-var vel : Vector2 = Vector2(0,0)
+var movement : Vector2 = Vector2(0,0)
 
 ## Functions
-# Get the movement input vectors
-func _unhandled_input(event) -> void:
-	if(is_player_1):
-		vel.x = Input.get_axis("player_1_move_left", "player_1_move_right")
-		vel.y = Input.get_axis("player_1_move_up", "player_1_move_down")
-	else:
-		vel.x = Input.get_axis("player_2_move_left", "player_2_move_right")
-		vel.y = Input.get_axis("player_2_move_up", "player_2_move_down")
-
 func _physics_process(delta) -> void:
-	# Set the velocity, and move the player
-	velocity = velocity.lerp(vel, delta * SLOWDOWN)
+	if(is_player_1):
+		movement = Input.get_vector("player_1_move_left", "player_1_move_right", "player_1_move_up", "player_1_move_down")
+	else:
+		movement = Input.get_vector("player_2_move_left", "player_2_move_right", "player_2_move_up", "player_2_move_down")
+	# Set the movementocity, and move the player
+	movement = movement * MOVEMENT_SPEED
+	velocity = velocity.lerp(movement, delta * SLOWDOWN)
 	move_and_slide()
