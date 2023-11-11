d398a5b9556906061c80844f5018311600bd547e
Alex Stewart <thealexstewart@protonmail.ch>
2023-11-11 18:24:48 -0400

    Items should now hopefully be able to exchange between the minecart and player

--- game/Minecart/MinecartMovement.gd
+++ game/Minecart/MinecartMovement.gd
@@ -4,7 +4,7 @@ extends RigidBody2D
 var vel : Vector2
 
 ## Functions
-func _integrate_forces(state):
+func _integrate_forces(state) -> void:
 	vel = state.get_linear_velocity()
 	vel.y = 0
 	state.set_linear_velocity(vel)
