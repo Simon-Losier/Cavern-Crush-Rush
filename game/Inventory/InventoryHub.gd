1b911bfa0e95419a46aebdd4110546466d7be878
DangerousDaniel <danielcox996@gmail.com>
2023-11-10 23:36:29 -0400

    Working Inventory without stacking

--- game/Inventory/InventoryHub.gd
+++ game/Inventory/InventoryHub.gd
@@ -2,14 +2,15 @@ extends Control
 
 @onready var player_inventory : PlayerInventory = $Inventory
 var item_data_ui_scene : PackedScene = preload("res://Inventory/ItemDataUI.tscn")
+@onready var h_box : HBoxContainer = $HBoxContainer
 
 func _ready():
 	create_inventory()
 		
 func create_inventory():
-	for inventory_item in player_inventory.inventory:
-		var instance_item = item_data_ui_scene
-		instance_item.set_ui_item_data(inventory_item)
-		add_child(instance_item)
+	for inventory_item_dict in player_inventory.inventory:
+		var item_instance = item_data_ui_scene.instantiate()
+		h_box.add_child(item_instance)
+		item_instance.set_ui_item_data(inventory_item_dict)
 	
 
--- game/Inventory/PlayerInventory.gd
+++ game/Inventory/PlayerInventory.gd
@@ -1,19 +1,13 @@
 class_name PlayerInventory extends Node2D
 
-@export var inventory : Array[Dictionary]
+@export var inventory : Array[ItemData]
 
 func add_item(item : ItemData):
-	var item_dict = {"key_stacksize" = 0, "key_item_data" = null} 
-	item_dict["key_stacksize"] += 1
-	item_dict["key_item_data"] = item
-	inventory.append(item_dict)
+	inventory.append(item)
 	
 func remove_item(item : ItemData):
 	for inventory_item in inventory:
-		if item == inventory_item["key_item_data"]:
-			if inventory_item["key_stacksize"] == 0:
-				inventory.erase(inventory_item)
-			else: 
-				inventory_item["key_stacksize"] -= 1
+		if item == inventory_item:
+			inventory.erase(inventory)
 
 
