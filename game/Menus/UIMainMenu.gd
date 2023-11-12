628d9cdbab251d79993af887df1cf8c74739c8f3
Alex Stewart <thealexstewart@protonmail.ch>
2023-11-12 15:18:49 -0400

    Background music, controls reverted

--- game/Menus/UIMainMenu.gd
+++ game/Menus/UIMainMenu.gd
@@ -1,7 +1,8 @@
 extends Control
 
+## Functions
 func _on_start_game_button_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Levels/GameScene.tscn")
 
 func _on_quit_game_button_pressed():
 	get_tree().quit()
