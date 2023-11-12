592bfb2a0c8235c70c971fe2f5792c3ef6c187e1
Watson <wdfwatson@gmail.com>
2023-11-12 15:50:05 -0400

    Added Bomb Effect

--- game/Items/bombArea.gd
+++ game/Items/bombArea.gd
@@ -4,10 +4,14 @@ extends Area2D
 var objToRemove = []
 
 @onready var bomb_sound = %BombSound
+@onready var explosion= load("res://Items/Explosion.tscn")
 
 ## Functions
 func _on_timer_timeout():
 	bomb_sound.play()
+	var exp = explosion.instantiate()
+	get_parent().add_child(exp)
+	exp.position = position
 	boom()
 
 func boom():
