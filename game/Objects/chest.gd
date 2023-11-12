8cf17f9881186f6174acc2b031c366a7f20dfb51
Alex Stewart <thealexstewart@protonmail.ch>
2023-11-12 15:03:56 -0400

    Chests should now give the right item. Bombs can now be placed by either player

--- game/Objects/chest.gd
+++ game/Objects/chest.gd
@@ -1,19 +1,15 @@
 extends Area2D
 
+## Variables
 @export var item: ItemData
 @export var player_inventory: PlayerInventory
+
 var open: bool = false
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+## Functions
+func _physics_process(delta):
 	if $AnimatedSprite2D.frame == 1:
 		$AnimatedSprite2D.pause()
-
-
 
 func _on_body_entered(body):
 	if (!open):
